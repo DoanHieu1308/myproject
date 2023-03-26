773f5705b5d26a51e5e4a2de768913f5fff652e5
DoanHieu1308 <128965111+DoanHieu1308@users.noreply.github.com>
2023-03-26 16:10:20 +0700

    message

---
+++ HocSinh.java
@@ -0,0 +1,31 @@
+
+public class HocSinh extends ConNguoi {
+    private String tenLop, tenTruong;
+
+	public HocSinh(String hoVaTen, int namSinh, String tenLop, String tenTruong) {
+		super(hoVaTen, namSinh);
+		this.tenLop = tenLop;
+		this.tenTruong = tenTruong;
+	}
+
+	public String getTenLop() {
+		return tenLop;
+	}
+
+	public void setTenLop(String tenLop) {
+		this.tenLop = tenLop;
+	}
+
+	public String getTenTruong() {
+		return tenTruong;
+	}
+
+	public void setTenTruong(String tenTruong) {
+		this.tenTruong = tenTruong;
+	}
+       
+    public void lamBaiTap()
+    {
+    	System.out.println("... . ....  ....");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+    public static void main(String[] args) {
+	ConNguoi cn = new ConNguoi("TITV", 2021);
+	cn.an();
+	cn.ngu();
+	cn.uong();
+	HocSinh hs = new HocSinh("Hieu", 2003,"A1H2o", "truong 1");
+	hs.an();
+	hs.uong();
+	hs.ngu();
+	hs.lamBaiTap();
+  }
+}
